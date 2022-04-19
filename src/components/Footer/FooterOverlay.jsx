8507d4e1d5d959f1d931019a7fac3538cbde5bb7
CrystalYM <crystalmjumi@gmail.com>
2022-04-19 12:58:37 +0100

    first commit

---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './FooterOverlay.css';
+
+const FooterOverlay = () => (
+  <div className='app__footerOverlay'>
+    <div className='app__footerOverlay_black'/>
+    <div className='app__footerOverlay_img app_bg'/>
+  </div>
+);
+
+export default FooterOverlay;
